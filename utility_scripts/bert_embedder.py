b63de68a968440653a12953d3a4a513e945f6578
Martín Villanueva <martin.villanueva@usm.cl>
2020-02-08 04:00:13 -0300

    Adds 2 sentence generalization to embedder

--- utility_scripts/bert_embedder.py
+++ utility_scripts/bert_embedder.py
@@ -90,6 +90,90 @@ def compute_input_arrays_qa(data, tokenizer, max_sequence_length):
             torch.tensor(input_segments, dtype=torch.long)]
 
 ##################################################################################################
+# functions to compute input arrays in bert format for a sentence pair: sentence1 & sentence2
+##################################################################################################
+
+def get_masks_2s(tokens, max_seq_length):
+    """Mask for padding"""
+    n_tokens = len(tokens)
+    return [1]*len(tokens) + [0]*(max_seq_length - n_tokens)
+
+def get_segments_2s(tokens, max_seq_length, sep_token):
+    """Segments: 0 for the first sequence, 1 for the second"""  
+    n_tokens = len(tokens)
+    first_sep_index = tokens.index(sep_token)
+    segments = [0]*(first_sep_index+1) + [1]*(n_tokens-first_sep_index-1) 
+    return segments + [0]*(max_seq_length - n_tokens)
+
+def get_ids_2s(tokens, tokenizer, max_seq_length):
+    """Token ids from Tokenizer vocab"""
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_ids = token_ids + [0] * (max_seq_length-len(token_ids))
+    return input_ids
+
+def trim_input_2s(sentence1_tokens, sentence2_tokens, max_sequence_length, 
+                  sentence1_max_len=254, sentence2_max_len=255):
+    sentence1_len = len(sentence1_tokens)
+    sentence2_len = len(sentence2_tokens)
+
+    if (sentence1_len+sentence2_len+3) > max_sequence_length:
+        if sentence1_len < sentence1_max_len:
+            sentence1_new_len = sentence1_len
+            sentence2_new_len = sentence2_max_len + (sentence1_max_len - sentence1_len)
+        elif sentence2_len < sentence2_max_len:
+            sentence2_new_len = sentence2_len
+            sentence1_new_len = sentence1_max_len + (sentence2_max_len - sentence2_len)
+        else:
+            sentence1_new_len = sentence1_max_len
+            sentence2_new_len = sentence2_max_len
+            
+        if sentence1_new_len+sentence2_new_len+3 != max_sequence_length:
+            print(len(sentence1_tokens))
+            print(len(sentence2_tokens))
+            print(sentence1_new_len)
+            print(sentence2_new_len)
+            raise ValueError(f"New sequence length should be {max_sequence_length} but is {sentence1_new_len+sentence2_new_len+3}")
+        
+        sentence1_tokens = sentence1_tokens[:sentence1_new_len]
+        sentence2_tokens = sentence2_tokens[:sentence2_new_len]
+    
+    return sentence1_tokens, sentence2_tokens
+
+def get_bert_inputs_2s(sentence1, sentence2, tokenizer, max_sequence_length, 
+                       s1_max_length, s2_max_length, sep_token):
+    """Converts tokenized input to ids, masks and segments for BERT"""
+    sentence1_tokens = tokenizer.tokenize(sentence1)
+    sentence2_tokens = tokenizer.tokenize(sentence2)
+    sentence1_tokens,sentence2_tokens = trim_input_2s(sentence1_tokens,
+                                                      sentence2_tokens,
+                                                      max_sequence_length, 
+                                                      s1_max_length, 
+                                                      s2_max_length)
+    all_tokens = ["[CLS]"] + sentence1_tokens  + [sep_token] + sentence2_tokens + ["[SEP]"]
+    input_ids = get_ids_2s(all_tokens, tokenizer, max_sequence_length)
+    input_masks = get_masks_2s(all_tokens, max_sequence_length)
+    input_segments = get_segments_2s(all_tokens, max_sequence_length, sep_token)
+    return [input_ids, input_masks, input_segments]
+
+def compute_input_arrays_2s(data, column1, column2, tokenizer, max_sequence_length,
+                            s1_max_length, s2_max_length, sep_token="[SEP]"):
+    input_ids, input_masks, input_segments = [], [], []
+    for _,row in tqdm(data.iterrows()):
+        ids,masks,segments = get_bert_inputs_2s(row[column1], 
+                                                row[column2], 
+                                                tokenizer, 
+                                                max_sequence_length,
+                                                s1_max_length, s2_max_length,
+                                                sep_token)
+        input_ids.append(ids)
+        input_masks.append(masks)
+        input_segments.append(segments)
+        
+    return [torch.tensor(input_ids, dtype=torch.long), 
+            torch.tensor(input_masks, dtype=torch.long), 
+            torch.tensor(input_segments, dtype=torch.long)]
+
+##################################################################################################
 # functions to compute input arrays in bert format for a sentence pair: title + question & answer
 ##################################################################################################
 
