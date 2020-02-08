3dcd1d59e4182fef36ee792288be560ca3c1ab84
Martín Villanueva <martin.villanueva@usm.cl>
2020-02-08 18:44:21 -0300

    Decreases batch_size of 2bertl

--- 2BERTL-tq+a-clspooling-bce.py
+++ 2BERTL-tq+a-clspooling-bce.py
@@ -95,7 +95,7 @@ NUM_FOLDS = 5
 DROPOUT = 0.2
 LEARNING_RATE = 1e-5
 EPOCHS = 25
-BATCH_SIZE = 3
+BATCH_SIZE = 2
 
 def train_epoch_bert(train_loader, model, optimizer, device, scheduler=None):
 	train_loss = 0
